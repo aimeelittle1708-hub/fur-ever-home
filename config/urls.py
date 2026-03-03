f7bfaa055c98e74c7f349513ea52d9afaf58e7ce
wouter-codes <wouter.klinkenberg@outlook.com>
2026-03-03 12:51:41 +0000

    Setup Django allauth and create style.css and script.js

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    path('', include("furever.urls")),
+    path('accounts/', include('allauth.urls')),
+    path('', include('furever.urls')),
 ]
