2b56f7e29888a2c697a4cc1157d37fee5c7c7eef
wouter-codes <wouter.klinkenberg@outlook.com>
2026-03-04 10:06:59 +0000

    Add pet viewing and detail views and templates and adjust urls

--- furever/urls.py
+++ furever/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('pets/', views.view_pets, name='view_pets'),
+    path('pets/<int:pet_id>/', views.pet_detail, name='pet_detail'),
 ]
--- furever/views.py
+++ furever/views.py
@@ -1,7 +1,36 @@
-from urllib import request
+from django.shortcuts import get_object_or_404, render
+from .models import Pet
 
-from django.shortcuts import render
-from django.http import HttpResponse 
 
 def home(request):
     return render(request, 'home.html')
+
+
+def view_pets(request):
+    pets = Pet.objects.filter(
+        status=Pet.Status.AVAILABLE,
+        authorised=True,
+    ).order_by('-featured', '-date_added')
+    return render(request, 'view_pets.html', {'pets': pets})
+
+
+def pet_detail(request, pet_id):
+    pet = get_object_or_404(
+        Pet,
+        pk=pet_id,
+        status=Pet.Status.AVAILABLE,
+        authorised=True,
+    )
+    return render(request, 'pet_detail.html', {'pet': pet})
+
+
+def request_pets(request):
+    return render(request, 'request_pets.html')
+
+
+def success_stories(request):
+    return render(request, 'success_stories.html')
+
+
+def about(request):
+    return render(request, 'about.html')
